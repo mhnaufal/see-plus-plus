18f2cbdf863be75d4c9f201ca38574dd0e39702a
mhnaufal <aal.pirates16@gmail.com>
2022-07-31 10:34:45 +0700

    [BASIC]: connect root folder with sub folder using cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+// #include "Basic-CPP/basic.h"
+#include <basic.h> // we can use this because we use `target_include_directories` in root CMakeLists.txt
 #include <iostream>
 
 int main() {
-    std::cout << "HOME" << std::endl;
-    return 0;
+  std::cout << "HOME" << std::endl;
+
+  /* BASIC */
+  create_response();
+
+  /* MODERN */
+
+  return 0;
 }
