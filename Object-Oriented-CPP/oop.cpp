21c5dc0ecf1b24609dbd354570448a2e0dd16f40
mhnaufal <aal.pirates16@gmail.com>
2022-08-03 07:05:49 +0700

    [OOP]: operator overloading

--- Object-Oriented-CPP/oop.cpp
+++ Object-Oriented-CPP/oop.cpp
@@ -1,4 +1,15 @@
 #include "oop.h"
 
-Pipe::Pipe() = default;
-Pipe::~Pipe() = default;
+Enemy::Enemy() = default;
+Enemy::Enemy(int x_coordinate, int y_coordinate, float spawn_time) {
+  x_coordinate_ = x_coordinate;
+  y_coordinate_ = y_coordinate;
+  spawn_time_ = spawn_time;
+}
+Enemy::~Enemy() = default;
+
+std::ostream &operator<<(std::ostream &os, const Enemy &enemy) {
+  return os << "x: " << enemy.get_x_coordinate()
+            << " y: " << enemy.get_y_coordinate()
+            << " spawn: " << enemy.get_spawn_time() << std::endl;
+}
--- Object-Oriented-CPP/oop.h
+++ Object-Oriented-CPP/oop.h
@@ -1,18 +1,27 @@
 #ifndef SHE_PLUS_PLUS_OOP_H
 #define SHE_PLUS_PLUS_OOP_H
 
-class Pipe {
+#include <iostream>
+
+class Enemy {
 public:
-  Pipe();  // default constructor
-  ~Pipe(); // destructor
-  [[nodiscard]] float get_spawn_time() const {
-    return spawn_time_;
-  }
+  Enemy(); // default constructor
+  Enemy(int x_coordinate, int y_coordinate, float spawn_time);
+  Enemy(Enemy &&var) = default;
+  Enemy(const Enemy &var) = default;
+  Enemy &operator=(Enemy &&var) = default;
+  Enemy &operator=(const Enemy &var) = default;
+  ~Enemy(); // destructor
+  [[nodiscard]] float get_spawn_time() const { return spawn_time_; }
+  [[nodiscard]] int get_x_coordinate() const { return x_coordinate_; }
+  [[nodiscard]] int get_y_coordinate() const { return y_coordinate_; }
 
 private:
   int x_coordinate_{}; // private data are ended with underscore "_"
   int y_coordinate_{};
   float spawn_time_{};
 };
+
+std::ostream &operator<<(std::ostream &os, const Enemy &enemy);
 
 #endif // SHE_PLUS_PLUS_OOP_H
